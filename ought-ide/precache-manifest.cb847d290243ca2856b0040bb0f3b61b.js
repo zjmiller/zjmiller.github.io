d0891fb1c9ac790d9c28274fd6989009719e9e29
Zachary Miller <zachary.john.miller@gmail.com>
2019-07-18 19:05:56 -0600

    Add ought-ide v4

---
+++ ought-ide/precache-manifest.cb847d290243ca2856b0040bb0f3b61b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce9035d40d54e9fc660e911734721943",
+    "url": "/ought-ide/index.html"
+  },
+  {
+    "revision": "17f03b3e301e6db6888c",
+    "url": "/ought-ide/static/js/2.3eb284d1.chunk.js"
+  },
+  {
+    "revision": "7e0402070bbca8620e35",
+    "url": "/ought-ide/static/js/main.d81775c3.chunk.js"
+  },
+  {
+    "revision": "16efe1ac42b328125ea3",
+    "url": "/ought-ide/static/js/runtime~main.99d42dbb.js"
+  }
+]);
